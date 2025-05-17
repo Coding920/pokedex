3f55b1a9e94f820d12b5c9a56330e45d95075808
coding920 <justin.tamez04@gmail.com>
2025-05-16 19:10:19 -0700

    Added exploreLocation func to pokeapi package

---
+++ internal/pokeapi/exploreLocation.go
@@ -0,0 +1,102 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type ExploreJson struct {
+	id                   int
+	name                 string
+	gameIndex            int
+	encounterMethodRates []struct {
+		encounterMethod struct {
+			name string
+			url  string
+		}
+		versionDetails []struct {
+			rate    int
+			version struct {
+				name string
+				url  string
+			}
+		}
+	}
+	location struct {
+		name string
+		url  string
+	}
+	names []struct {
+		name     string
+		language struct {
+			name string
+			url  string
+		}
+	}
+	pokemonEncounters []struct {
+		pokemon struct {
+			name string
+			url  string
+		}
+		versionDetails []struct {
+			version struct {
+				name string
+				url  string
+			}
+			maxChance        int
+			encounterDetails []struct {
+				minLevel        int
+				maxLevel        int
+				conditionValues []any
+				chance          int
+				method          struct {
+					name string
+					url  string
+				}
+			}
+		}
+	}
+}
+
+func (c *PokeClient) exploreLocation(location string) (ExploreJson, error) {
+	endpoint := baseApi + "location-area/" + location
+
+	if data, ok := c.cache.Get(endpoint); ok {
+		var jsonData ExploreJson
+		err := json.Unmarshal(data, &jsonData)
+		if err != nil {
+			return ExploreJson{}, err
+		}
+		return jsonData, nil
+	}
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return ExploreJson{}, err
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return ExploreJson{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return ExploreJson{}, fmt.Errorf("Server response not OK, response: %v", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ExploreJson{}, err
+	}
+	c.cache.Add(endpoint, data)
+
+	var jsonData ExploreJson
+	err = json.Unmarshal(data, &jsonData)
+	if err != nil {
+		return ExploreJson{}, err
+	}
+
+	return jsonData, nil
+}
